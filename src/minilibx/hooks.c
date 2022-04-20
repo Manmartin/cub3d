85c10468e96b89487b5dc9acfb6e94f51b47adf9
Manmartin <manuelmartinlopez02@gmail.com>
2022-04-20 17:35:17 +0200

    Some missing files

--- src/minilibx/hooks.c
+++ src/minilibx/hooks.c
@@ -1,37 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   hooks.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: manmarti <manmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/20 17:28:55 by manmarti          #+#    #+#             */
-/*   Updated: 2022/04/20 17:33:40 by manmarti         ###   ########.fr       */
+/*   Created: 2022/04/20 17:15:07 by manmarti          #+#    #+#             */
+/*   Updated: 2022/04/20 17:27:55 by manmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
+#include <cub3d.h>
 
-# include <unistd.h>
-# include <stdio.h>
-
-# include <mlx.h>
-
-typedef struct s_img {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_img;
-
-typedef struct s_data {
-	void	*mlx;
-	void	*win;
-}				t_data;
-
-/* hooks.c */
-int	keys_hook(int keycode, t_data *data);
-
-#endif
+int	keys_hook(int keycode, t_data *data)
+{
+	(void)data;
+	printf("Key number = %i\n", keycode);
+	return (keycode);
+}
