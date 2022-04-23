bb85721df4f81fec4597bac617641c437e4e2f0a
Manuel Martin lopez <manmarti@c3r2s4.42madrid.com>
2022-04-23 20:34:30 +0200

    fix: red cross now works correctly

--- src/minilibx/main_loop.c
+++ src/minilibx/main_loop.c
@@ -6,7 +6,7 @@
 /*   By: manmarti <manmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 18:27:38 by manmarti          #+#    #+#             */
-/*   Updated: 2022/04/23 19:04:53 by manmarti         ###   ########.fr       */
+/*   Updated: 2022/04/23 20:33:31 by manmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,10 @@
 
 void	main_loop(t_data *data)
 {
-	t_img	img;
-
 	data->mlx = mlx_init();
 	data->win = mlx_new_window(data->mlx, 1920, 1080, "Cub3d");
-	img.img = mlx_new_image(data->mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-			&img.endian);
 	mlx_do_key_autorepeaton(data->mlx);
 	mlx_hook(data->win, 2, 1L, keys_hook, &data);
-	mlx_put_image_to_window(data->mlx, data->win, img.img, 0, 0);
+	mlx_hook(data->win, 17, 0, clean_exit, &data);
 	mlx_loop(data->mlx);
 }
