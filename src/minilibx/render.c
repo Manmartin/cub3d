65f1909052446608d88e587de70f0707bdd795d9
Manuel Martin lopez <manmarti@c3r4s3.42madrid.com>
2022-04-25 20:14:42 +0200

    feature: Render squares

--- src/minilibx/render.c
+++ src/minilibx/render.c
@@ -6,7 +6,7 @@
 /*   By: manmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 19:34:54 by manmarti          #+#    #+#             */
-/*   Updated: 2022/04/25 19:52:59 by manmarti         ###   ########.fr       */
+/*   Updated: 2022/04/25 20:13:38 by manmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 void put_pixel(t_img *img, int x, int y, int color)
 {
-	(void)img;
-	(void)x;
-	(void)y;
-	(void)color;	
+	char	*dst;
+
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	put_sqrt(int x, int y, int size, t_img *img)
+{
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
+			put_pixel(img, x + j, y + i, 255);
 }
 
 
@@ -25,5 +32,6 @@ void	render(t_data *data)
 {
 	(void)data;
 	mlx_clear_window(data->mlx, data->win);
+	put_sqrt(10, 10, 50, &data->img);
 	mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
 }
