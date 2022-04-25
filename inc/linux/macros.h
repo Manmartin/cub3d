e7d11d6b0038c96195939091087b6e0c97a0e914
manmartin <manuelmartinlopez02@gmail.com>
2022-04-25 22:00:29 +0200

    feature: full functional linux version

---
+++ inc/linux/macros.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   macros.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manmarti <manmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 17:01:57 by manmarti          #+#    #+#             */
+/*   Updated: 2022/04/23 17:08:56 by manmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MACROS_H
+# define MACROS_H
+
+# define KEY_ESCAPE	    		65307
+# define KEY_W				119
+# define KEY_A				97
+# define KEY_S				115
+# define KEY_D				100
+# define KEY_LEFT			65361
+# define KEY_RIGHT			65363
+
+#endif
