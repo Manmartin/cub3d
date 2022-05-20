ebc553e120037a6882973b00b66f175b596865d4
manmartin <manuelmartinlopez02@gmail.com>
2022-05-20 19:39:02 +0200

    bugfix: Macros WIDTH and HEIGHT now works

--- src/minilibx/main_loop.c
+++ src/minilibx/main_loop.c
@@ -6,7 +6,7 @@
 /*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 18:27:38 by manmarti          #+#    #+#             */
-/*   Updated: 2022/05/20 18:22:11 by manuel           ###   ########.fr       */
+/*   Updated: 2022/05/20 19:21:27 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ void	main_loop(t_data *data)
 	t_img	img;
 
 	data->mlx = mlx_init();
-	data->win = mlx_new_window(data->mlx, 800, 800, "Cub3d");
-	img.img = mlx_new_image(data->mlx, 800, 800);
+	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "Cub3d");
+	img.img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 			&img.endian);
 	data->img = img;
