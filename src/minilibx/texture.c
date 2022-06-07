26b791c962bf05840241257f4e4981b9fdaa5d87
Manuel Martin lopez <manmarti@c3r4s1.42madrid.com>
2022-06-07 12:00:05 +0200

    normminette: minilibx

--- src/minilibx/texture.c
+++ src/minilibx/texture.c
@@ -6,7 +6,7 @@
 /*   By: manmarti <manmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 09:25:17 by manuel            #+#    #+#             */
-/*   Updated: 2022/06/07 11:06:42 by manmarti         ###   ########.fr       */
+/*   Updated: 2022/06/07 11:54:15 by manmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ unsigned int	get_pixel(t_img *img, int x, int y)
 	return (*(unsigned int *)dst);
 }
 
+/* Esta funcion va fuera de aqui */
+
 void	load_textures(t_data *data)
 {
 	int		i;
@@ -30,59 +32,58 @@ void	load_textures(t_data *data)
 	{
 		tex = &data->scene->tex_img[i];
 		tex->img = mlx_xpm_file_to_image(data->mlx, data->scene->textures[i],
-			&tex->width, &tex->height);
+				&tex->width, &tex->height);
 		if (!tex->img)
 		{
 			printf("Error\n");
 			exit(0);
 		}
-		tex->addr = mlx_get_data_addr(tex->img, 
-			&tex->bits_per_pixel, &tex->line_length, &tex->endian);
+		tex->addr = mlx_get_data_addr(tex->img,
+				&tex->bits_per_pixel, &tex->line_length, &tex->endian);
 	}
+}
+
+static t_tex	init_texture(t_vec *player, t_img img, t_ray ray, int side)
+{
+	t_tex	tex;
+
+	if (side == NORTH || side == SOUTH)
+		tex.wall_x = (player->x + 0.5) + ray.length * ray.x;
+	else
+		tex.wall_x = (player->y + 0.5) + ray.length * ray.y;
+	tex.wall_x -= floor(tex.wall_x);
+	tex.tex_x = floor(tex.wall_x * (double)(img.width));
+	if (side == WEST || side == SOUTH)
+		tex.tex_x = img.width - tex.tex_x - 1;
+	tex.step = 1.0 * img.height / ray.height;
+	tex.tex_pos = (ray.start - HEIGHT / 2 + ray.height / 2) * tex.step;
+	return (tex);
 }
 
 void	put_texture(t_data	*data, t_ray ray, int side)
 {
-	t_vec	*player;
-	t_img	img;
-	double	wallX;
-	int		texX;
+	t_tex			tex;
+	int				y;
+	unsigned int	color;
+	int				index;
 
-	player = data->game->player;
-	img = data->scene->tex_img[side];
-	if (side == NORTH || side == SOUTH)
-		wallX = (player->x + 0.5) + ray.length * ray.x;
-	else
-		wallX = (player->y + 0.5) + ray.length * ray.y;
-	wallX -= floor(wallX);
-	texX = floor(wallX * (double)(img.width));
-	if (side == WEST || side == SOUTH)
-		texX = img.width - texX - 1;
-	double	step;
-
-	step = 1.0 * img.height / ray.height;
-
-	double	texPos;
-	texPos = (ray.start - HEIGHT / 2 + ray.height / 2) * step;
-	unsigned int color;
-
-	int y;
-
+	tex = init_texture(data->game->player, data->scene->tex_img[side],
+			ray, side);
 	y = -1;
 	while (++y < ray.start)
 		put_pixel(&data->img, ray.column, y, data->scene->floor_color);
 	while (y < ray.end)
 	{
-		int texY = texPos;
-		texPos += step;
-		int index = texY * img.width  + texX;
-		color = ((unsigned int *)(img.addr))[index];
+		tex.tex_y = tex.tex_pos;
+		tex.tex_pos += tex.step;
+		index = tex.tex_y * data->scene->tex_img[side].width + tex.tex_x;
+		color = ((unsigned int *)(data->scene->tex_img[side].addr))[index];
 		put_pixel(&data->img, ray.column, y, color);
 		y++;
 	}
 	while (y < HEIGHT)
 	{
-	put_pixel(&data->img, ray.column, y, data->scene->ceilling_color);
+		put_pixel(&data->img, ray.column, y, data->scene->ceilling_color);
 		y++;
 	}
 }
